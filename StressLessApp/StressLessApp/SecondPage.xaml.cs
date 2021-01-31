96c4a91f53162cdeae245719bcf24fe29d1db748
Gracyn <gracyngreen@gmail.com>
2021-01-31 09:45:20 -0600

    * SecondPage.xaml: * HomePage.xaml.cs: * SecondPage.xaml.cs:
    * HomePage.xaml: Made a time function and a button to switch to the
      calendar view.

---
+++ StressLessApp/StressLessApp/SecondPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StressLessApp
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePage());
+        }
+    }
+}
