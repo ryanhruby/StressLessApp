0808f224e546bbff95e25b76f047f394347d4809
ryanhruby <ryanhruby549@gmail.com>
2021-01-31 05:27:32 -0600

    Added Sleep and Food pages with buttons

---
+++ StressLessApp/StressLessApp/FoodPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StressLessApp
+{
+    public partial class FoodPage : ContentPage
+    {
+        public FoodPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NextPageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SleepPage());
+        }
+    }
+}
