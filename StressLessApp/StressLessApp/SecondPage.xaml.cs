8f42997f7cfdc9073bb3547f36ecd9a9037583a2
Gracyn <gracyngreen@gmail.com>
2021-01-31 10:08:24 -0600

    * SecondPage.xaml:
    * SecondPage.xaml.cs: Started to add events to the calendar based off
      of the details the user inputed.

--- StressLessApp/StressLessApp/SecondPage.xaml.cs
+++ StressLessApp/StressLessApp/SecondPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Xamarin.Essentials;
+
 using Xamarin.Forms;
 
 namespace StressLessApp
@@ -10,6 +12,10 @@ namespace StressLessApp
         public SecondPage()
         {
             InitializeComponent();
+
+            double hoursToStudy = (Convert.ToDouble(Preferences.Get("credit_hours", "")) * 2) / 7.0;
+
+            MondayText.Text = "You should study at least " + hoursToStudy + " hours today.";
         }
         private async void Button_Clicked(System.Object sender, System.EventArgs e)
         {
